cb3e273c9f6d56a8270cbeffc21d70cc0b98284c
pReet2 <preetiitzme@gmail.com>
2021-03-02 19:21:25 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,59 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { DetailComponent } from './posts/create/detail/detail.component';
+import { SubjectComponent } from './posts/create/detail/subject/subject.component';
+import { InfoComponent } from './posts/create/detail/subject/info/info.component';
+import { CreateComponent } from './posts/create/create.component';
 
-const routes: Routes = [];
+
+
+
+  const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'posts',
+        children: [
+          {
+            path: '',
+            component: PostsComponent
+          },
+          {
+              path: 'create',
+              component: CreateComponent,
+              children:[
+                {
+                  path: 'detail',
+                  component: DetailComponent, 
+                  children:[
+                    {
+                      path: 'subject',
+                      component: SubjectComponent, 
+                      children:[
+                        {
+                          path: 'info',
+                          component: InfoComponent, 
+                      }
+               
+                      ]
+        
+                  }
+           
+                  ]
+    
+              }
+       
+              ]
+          },
+            ]
+    }
+  ];
+  
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreateComponent } from './posts/create/create.component';
+import { DetailComponent } from './posts/create/detail/detail.component';
+import { SubjectComponent } from './posts/create/detail/subject/subject.component';
+import { InfoComponent } from './posts/create/detail/subject/info/info.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CreateComponent,
+    DetailComponent,
+    SubjectComponent,
+    InfoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
